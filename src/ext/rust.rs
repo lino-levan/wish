4d69091e1c1d24ab9e7d54ca9e5f74a85e967f8a
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-11-28 10:40:13 -0800

    chore: wait this is actually getting pretty far

--- src/ext/rust.rs
+++ src/ext/rust.rs
@@ -1,9 +1,9 @@
 use crate::ext::util::{exists, run_command};
 
-pub fn do_rust(args: &Vec<&str>) {
+pub fn do_rust(args: &Vec<&str>) -> bool {
     // Check if there is a `Cargo.toml`, if not, quit.
     if !exists("Cargo.toml") {
-        return;
+        return false;
     }
 
     let first = args.get(0);
@@ -12,12 +12,16 @@ pub fn do_rust(args: &Vec<&str>) {
         Some(val) => match *val {
             "run" => {
                 run_command("cargo", vec!["run"]);
+                return true;
             }
             "build" => {
                 run_command("cargo", vec!["build"]);
+                return true;
             }
             _ => {}
         },
         _ => {}
     }
+
+    return false;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod ext;
 use ext::rust::do_rust;
 
 fn main() {
+    let extensions = vec![do_rust];
+
     let mut input_string = String::new();
 
-    while input_string != "exit" {
+    'shell_loop: loop {
         // Print the prompt
         print!("$ ");
         std::io::stdout().flush().unwrap();
@@ -16,8 +18,43 @@ fn main() {
         std::io::stdin().read_line(&mut input_string).unwrap();
         input_string = input_string.trim().to_string();
 
+        // Ignore any text after #
+        if input_string.contains("#") {
+            input_string = input_string.split("#").collect::<Vec<&str>>()[0].to_string();
+        }
+
         // Run the input
         let input_vec: Vec<&str> = input_string.split(" ").collect();
-        do_rust(&input_vec.to_vec());
+
+        // If the input is empty, continue the loop.
+        if input_vec.len() == 0 || input_vec[0] == "" {
+            continue 'shell_loop;
+        }
+
+        // Check for built-in commands
+        if input_vec[0] == "exit" {
+            break 'shell_loop;
+        }
+
+        // Check if binary exists in path
+        if which::which(input_vec[0]).is_ok() {
+            let mut child = std::process::Command::new(input_vec[0])
+                .args(&input_vec[1..])
+                .spawn()
+                .unwrap();
+
+            child.wait().unwrap();
+            continue 'shell_loop;
+        }
+
+        // Run "magic" extensions
+        for extension in &extensions {
+            if extension(&input_vec) {
+                continue 'shell_loop;
+            }
+        }
+
+        // If we get here, the command was not found.
+        println!("Command not found: {}", input_string);
     }
 }
