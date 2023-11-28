093456a4b658c793c90f49cfac79fb8c994a8c52
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-11-28 07:52:54 -0800

    chore: make it actually work okay

--- src/ext/util.rs
+++ src/ext/util.rs
@@ -1,11 +1,12 @@
 use std::fs::metadata;
-use std::io::{stderr, stdout, Write};
 use std::process::Command;
 
 pub fn run_command(name: &str, args: Vec<&str>) {
-    let output = Command::new(name).args(args).output().unwrap();
-    stdout().write_all(output.stdout.as_slice()).unwrap();
-    stderr().write_all(output.stderr.as_slice()).unwrap();
+    // spawn child process
+    let mut child = Command::new(name).args(args).spawn().unwrap();
+
+    // wait for child to finish
+    child.wait().unwrap();
 }
 
 pub fn exists(path: &str) -> bool {
